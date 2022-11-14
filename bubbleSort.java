126af46860c376a8c1626912fd510c3d3c3d0a1d
chitrakarayat <73735245+chitrakarayat@users.noreply.github.com>
2022-11-14 10:20:39 +0530

    Create bubbleSort.java

---
+++ bubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class bubbleSort {
+
+
+    // key points
+    // compare adjacent elements
+    // after every iteration the largest element in the sub-array reaches the end
+    // Time Complexity O(n^2)
+    // Space Complexity O(1)
+    // Number of comparisons in each step n-i
+    // Total number of comparisons n(n-1)/2
+
+    public static void bubbleSort(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j< arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
